8b8002ccaed750357004b7f7626e405cc97080c4
Alpay Oğuzhan Palabıyık <alpayoguz97@gmail.com>
2022-05-13 12:47:52 +0300

    productlist page localstorage bug fixed

--- src/pages/ProductList.js
+++ src/pages/ProductList.js
@@ -1,12 +1,26 @@
-import React from 'react'
+import React, {useEffect} from 'react'
 import Announcement from '../components/Announcement'
 import Navbar from '../components/Navbar'
 import Products from '../components/Products'
 import Filters from '../components/Filters'
 import { useFilter } from '../contexts/FilterContext'
+import { useCart } from '../contexts/CartContext'
 
 const ProductList = () => {
    const {filteredProducts, setFilteredProducts} = useFilter()
+     const {cartItems, setCartItems} = useCart()
+
+
+   useEffect(()=>{
+      if(cartItems?.length > 0){
+        localStorage.setItem("cartItems", JSON.stringify(cartItems))
+        
+      }else{
+        localStorage.setItem("cartItems", JSON.stringify([]))
+      }
+     
+      
+    },[cartItems])
   return (
      <>
      <div>
